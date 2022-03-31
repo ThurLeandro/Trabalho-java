080dad7dabd55b987c90ad145dbb6e074fc5d855
ThurLeandro <arturben102@gmail.com>
2022-03-31 11:19:23 -0300

    Trabalho de java

---
+++ src/Questão_1.java
@@ -0,0 +1,16 @@
+
+public class Questão_1 {
+
+	public static void main(String[]args) {
+		 int raio = 2;
+	    double PI = 3.1416; 
+		double area = PI *(raio*raio);
+		System.out.println("Valor da area: "+ area);
+		
+		
+		
+		
+	}
+	
+	
+}
---
+++ src/Questão_2_a.java
@@ -0,0 +1,22 @@
+
+public class Questão_2_a {
+
+	public static void main(String[] args) {
+		double a = 4.2;
+		double b = 5.6;
+		double c = 8.7;
+		double PI = 3.14159 ;
+		double areat = (a * c) / 2;
+		double areac = PI * (c*c);
+		double areatr = (a + b) * c / 2 ; 
+		double areaq = b * b ;
+		double arear = a * b;
+		System.out.println("area do triangulo: " + areat);
+		System.out.println("area do circulo: " + areac);
+		System.out.println("area do trapezio: " + areatr);
+		System.out.println("area do quadrado: " + areaq);
+		System.out.println("area do retãngulo: " + arear);
+		
+	}
+	
+}
